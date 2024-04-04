e618617e9759a2a4bb6d7290f3e1b7f429e9a9f7
Marek Vajgl <engin@seznam.cz>
2024-04-04 22:34:30 +0200

    Renamed typo in ELoging->ELogging
    Created _Release folder

---
+++ updateRelease.bat
@@ -0,0 +1,18 @@
+set ES=.
+set OUT=.\_Release\
+
+copy %ES%\ESystem\bin\debug\net6.0\esystem.dll %OUT%
+copy %ES%\ESystem\bin\debug\net6.0\esystem.pdb %OUT%
+
+copy %ES%\ELogging\bin\Debug\net6.0\ELogging.dll %OUT%
+copy %ES%\ELogging\bin\debug\net6.0\ELogging.pdb %OUT%
+copy %ES%\ELogging\bin\debug\net6.0\ELogging.deps.json %OUT%
+
+copy %ES%\ESystem.WPF\bin\debug\net6.0-windows\esystem.wpf.dll %OUT%
+copy %ES%\ESystem.WPF\bin\debug\net6.0-windows\esystem.wpf.pdb %OUT%
+
+copy %ES%\ESimConnect\bin\debug\net6.0-windows\ESimConnect.dll %OUT%
+copy %ES%\ESimConnect\bin\debug\net6.0-windows\ESimConnect.pdb %OUT%
+copy %ES%\ESimConnect\bin\debug\net6.0-windows\ESimConnect.deps.json %OUT%
+copy %ES%\ESimConnect\DLLs\simconnect.dll %OUT%
+copy %ES%\ESimConnect\bin\debug\net6.0-windows\Microsoft.FlightSimulator.SimConnect.dll %OUT%
