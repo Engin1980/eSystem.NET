5a747fa3e5a1231a97189c14f6e9f45832998063
Marek Vajgl <engin@seznam.cz>
2024-05-03 18:10:40 +0200

    Add UnexpectedEnumValueException and ToDoException

---
+++ ESystem/Exceptions/ToDoException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESystem.Exceptions
+{
+    /// <summary>
+    /// Supposed to be invoked on code, which should be implemented in near future.
+    /// </summary>
+    public class ToDoException : Exception
+    {
+        public ToDoException()
+        {
+        }
+
+        public ToDoException(string? message) : base(message)
+        {
+        }
+    }
+}
---
+++ ESystem/Exceptions/UnexpectedEnumValueException.cs
@@ -0,0 +1,24 @@
+﻿using ESystem.Asserting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESystem.Exceptions
+{
+    /// <summary>
+    /// Supposed to be invoked in switch/cases, when unexpected not-null value occurs.
+    /// </summary>
+    public class UnexpectedEnumValueException : Exception
+    {
+        public object EnumValue { get; private set; }
+        public Type EnumType { get; private set; }
+        public UnexpectedEnumValueException(Enum unexpectedEnumValue) : base($"Unexpected enum value '{unexpectedEnumValue}' for type '{unexpectedEnumValue.GetType().FullName}'.")
+        {
+            EAssert.Argument.IsNotNull(unexpectedEnumValue, nameof(unexpectedEnumValue));
+            this.EnumValue = unexpectedEnumValue;
+            this.EnumType = unexpectedEnumValue.GetType();
+        }
+    }
+}
