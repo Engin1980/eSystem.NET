9e06fd7adfc9d780e47561017084502c3f592eaa
Marek Vajgl <engin@seznam.cz>
2024-05-06 23:29:56 +0200

    Add EXmlLib to releases.

--- _updateRelease.bat
+++ _updateRelease.bat
@@ -13,4 +13,7 @@ copy %ES%\ELogging\bin\debug\net6.0\ELogging.deps.json %OUT%
 copy %ES%\ESystem.WPF\bin\debug\net6.0-windows\esystem.wpf.dll %OUT%
 copy %ES%\ESystem.WPF\bin\debug\net6.0-windows\esystem.wpf.pdb %OUT%
 
+copy %ES%\EXmlLib\bin\debug\net6.0-windows\EXmlLib.dll %OUT%
+copy %ES%\EXmlLib\bin\debug\net6.0-windows\EXmlLib.pdb %OUT%
+
 pause
