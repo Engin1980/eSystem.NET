9f3b97d9a362c5e2838045ac87913dbaf71643ed
Marek Vajgl <engin@seznam.cz>
2025-04-18 00:10:00 +0200

    Added structs for Distance / Heading / Speed.

---
+++ ESystem/Structs/Distance.cs
@@ -0,0 +1,130 @@
+﻿using ESystem.Asserting;
+using ESystem.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESystem.Structs
+{
+  /// <summary>
+  /// Enum representing different units of distance.
+  /// </summary>
+  public enum DistanceUnit
+  {
+    /// <summary>
+    /// Millimeters (1 mm = 0.001 meters).
+    /// </summary>
+    Millimeters,  // Millimeters
+
+    /// <summary>
+    /// Meters (the base unit of length in the International System of Units).
+    /// </summary>
+    Meters,       // Meters
+
+    /// <summary>
+    /// Kilometers (1 km = 1000 meters).
+    /// </summary>
+    Kilometers,   // Kilometers
+
+    /// <summary>
+    /// Miles (1 mile = 1609.34 meters).
+    /// </summary>
+    Miles,        // Miles
+
+    /// <summary>
+    /// Nautical miles (1 nautical mile = 1852 meters).
+    /// </summary>
+    NauticalMiles, // Nautical miles
+
+    /// <summary>
+    /// Feet (1 foot = 0.3048 meters).
+    /// </summary>
+    Feet          // Feet
+  }
+
+  public readonly struct Distance
+  {
+    public double Value { get; }
+    public DistanceUnit Unit { get; }
+
+    public Distance(double value, DistanceUnit unit)
+    {
+      EAssert.Argument.IsTrue(value >= 0, nameof(value), $"Distance cannot be negative (provided: {value}).");
+      Value = value;
+      Unit = unit;
+    }
+
+    public override string ToString()
+    {
+      return $"{Value} {Unit}";
+    }
+
+    public Distance To(DistanceUnit targetUnit)
+    {
+      if (Unit == targetUnit)
+        return this;
+
+      double valueInMeters = Unit switch
+      {
+        DistanceUnit.Millimeters => Value / 1000,
+        DistanceUnit.Meters => Value,
+        DistanceUnit.Kilometers => Value * 1000,
+        DistanceUnit.Miles => Value * 1609.34,
+        DistanceUnit.NauticalMiles => Value * 1852,
+        DistanceUnit.Feet => Value * 0.3048,
+        _ => throw new UnexpectedEnumValueException(Unit)
+      };
+
+      double newValue = targetUnit switch
+      {
+        DistanceUnit.Millimeters => valueInMeters * 1000,
+        DistanceUnit.Meters => valueInMeters,
+        DistanceUnit.Kilometers => valueInMeters / 1000,
+        DistanceUnit.Miles => valueInMeters / 1609.34,
+        DistanceUnit.NauticalMiles => valueInMeters / 1852,
+        DistanceUnit.Feet => valueInMeters / 0.3048,
+        _ => throw new UnexpectedEnumValueException(Unit)
+      };
+
+      return new Distance(newValue, targetUnit);
+    }
+
+    public static Distance operator +(Distance a, Distance b)
+    {
+      var bConverted = b.To(a.Unit);
+      return new Distance(a.Value + bConverted.Value, a.Unit);
+    }
+
+    public static Distance operator -(Distance a, Distance b)
+    {
+      var bConverted = b.To(a.Unit);
+      return new Distance(a.Value - bConverted.Value, a.Unit);
+    }
+
+    public static Distance operator *(Distance d, double scalar) => new(d.Value * scalar, d.Unit);
+
+    public static Distance operator *(double scalar, Distance d) => d * scalar;
+
+    public static Distance operator /(Distance d, double scalar) => new(d.Value / scalar, d.Unit);
+
+    public static bool operator ==(Distance a, Distance b) => a.To(DistanceUnit.Meters).Value == b.To(DistanceUnit.Meters).Value;
+
+    public static bool operator !=(Distance a, Distance b) => !(a == b);
+
+    public static bool operator <(Distance a, Distance b) => a.To(DistanceUnit.Meters).Value < b.To(DistanceUnit.Meters).Value;
+
+    public static bool operator <=(Distance a, Distance b) => a.To(DistanceUnit.Meters).Value <= b.To(DistanceUnit.Meters).Value;
+
+    public static bool operator >(Distance a, Distance b) => a.To(DistanceUnit.Meters).Value > b.To(DistanceUnit.Meters).Value;
+
+    public static bool operator >=(Distance a, Distance b) => a.To(DistanceUnit.Meters).Value >= b.To(DistanceUnit.Meters).Value;
+
+    public override bool Equals(object? obj) => obj is Distance distance && this == distance;
+
+    public override int GetHashCode() => HashCode.Combine(Value, Unit);
+
+    public static implicit operator double(Distance distance) => distance.Value;
+  }
+}
---
+++ ESystem/Structs/Speed.cs
@@ -0,0 +1,121 @@
+﻿using ESystem.Asserting;
+using ESystem.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESystem.Structs
+{
+  /// <summary>
+  /// Enum representing different units of speed.
+  /// </summary>
+  public enum SpeedUnit
+  {
+    /// <summary>
+    /// Kilometers per hour.
+    /// </summary>
+    KPH,   // Kilometers per hour
+
+    /// <summary>
+    /// Miles per hour.
+    /// </summary>
+    MPH,   // Miles per hour
+
+    /// <summary>
+    /// Knots (nautical miles per hour).
+    /// </summary>
+    KTS,   // Knots (nautical miles per hour)
+
+    /// <summary>
+    /// Meters per second.
+    /// </summary>
+    MPS,   // Meters per second
+
+    /// <summary>
+    /// Feet per second.
+    /// </summary>
+    FPS,   // Feet per second
+
+    /// <summary>
+    /// Feet per minute.
+    /// </summary>
+    FPM    // Feet per minute
+  }
+
+  public readonly struct Speed
+  {
+    public double Value { get; }
+    public SpeedUnit Unit { get; }
+
+    public Speed(double value, SpeedUnit unit)
+    {
+      EAssert.Argument.IsTrue(value >= 0, nameof(value), $"Speed cannot be negative (provided: {value}.");
+      Value = value;
+      Unit = unit;
+    }
+
+    public override string ToString() =>
+        $"{Value} {Unit}";
+
+    public Speed To(SpeedUnit targetUnit)
+    {
+      if (Unit == targetUnit)
+        return this;
+
+      // Convert to meters per second first
+      double valueInMPS = Unit switch
+      {
+        SpeedUnit.KPH => Value / 3.6,
+        SpeedUnit.MPH => Value * 0.44704,
+        SpeedUnit.KTS => Value * 0.514444,
+        SpeedUnit.MPS => Value,
+        SpeedUnit.FPS => Value * 0.3048,  // Convert FPS to MPS
+        SpeedUnit.FPM => Value / 60 * 0.3048, // Convert FPM to MPS
+        _ => throw new UnexpectedEnumValueException(Unit)
+      };
+
+      // Then convert from meters per second to target unit
+      double newValue = targetUnit switch
+      {
+        SpeedUnit.KPH => valueInMPS * 3.6,
+        SpeedUnit.MPH => valueInMPS / 0.44704,
+        SpeedUnit.KTS => valueInMPS / 0.514444,
+        SpeedUnit.MPS => valueInMPS,
+        SpeedUnit.FPS => valueInMPS / 0.3048,  // Convert MPS to FPS
+        SpeedUnit.FPM => valueInMPS / 0.3048 * 60,  // Convert MPS to FPM
+        _ => throw new UnexpectedEnumValueException(Unit)
+      };
+
+      return new Speed(newValue, targetUnit);
+    }
+
+    public static implicit operator double(Speed speed) => speed.Value;
+
+    public static Speed operator +(Speed speed, double value) => new(speed.Value + value, speed.Unit);
+
+    public static Speed operator -(Speed speed, double value) => new(speed.Value - value, speed.Unit);
+
+    public static Speed operator *(Speed speed, double value) => new(speed.Value * value, speed.Unit);
+
+    public static Speed operator /(Speed speed, double value) => new(speed.Value / value, speed.Unit);
+
+    public static bool operator ==(Speed speed, double value) => speed.Value == value;
+
+    public static bool operator !=(Speed speed, double value) => speed.Value != value;
+
+    public static bool operator <(Speed speed, double value) => speed.Value < value;
+
+    public static bool operator >(Speed speed, double value) => speed.Value > value;
+
+    public static bool operator <=(Speed speed, double value) => speed.Value <= value;
+
+    public static bool operator >=(Speed speed, double value) => speed.Value >= value;
+
+    public override bool Equals(object? obj) => obj is Speed speed && this == speed;
+
+    public override int GetHashCode() => HashCode.Combine(Value, Unit);
+  }
+
+}
